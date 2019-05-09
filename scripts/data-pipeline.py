44149d6a87ecdd1812fbebfd1dbc2f3c014c125f
Oliver Kröning <oliver.kroening@gmx.de>
2019-05-09 13:25:06 +0200

    finish ML pipeline, create train.py

--- scripts/data-pipeline.py
+++ scripts/data-pipeline.py
@@ -1,9 +1,33 @@
-# import packages
-import sys
+# import libraries
+import pandas as pd
+import numpy as np
+import re
+from time import time
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 
-
-def load_data(data_file):
+def load_data(data_file, table_name):
+    '''
+    Function:   load data from SQLite database and return X and y variables
+    Args:
+        data_file (str):    path of SQLite database
+        table_name (str):   name of table in SQLite database
+    Return:
+        X (pd.DataFrame):   messages (features)
+        y (pd.DataFrame):   categories (labels)
+    '''
     # read in file
+
 
 
     # clean data
